dcff75c0a8843110948af7cb7baf2f31daf27063
fbarreir <fh.barreiro@gmail.com>
2022-01-27 11:34:28 +0100

    Check the lock for unmounting

--- docker/cvmfs/check.sh
+++ docker/cvmfs/check.sh
@@ -11,28 +11,27 @@ do
 	for mp in `echo ${MOUNT_REPOS} |tr , ' '` ; do
     echo "`date`: checking /cvmfs/${mp}"
 
-     if [ `df -h | grep ${mp} | wc -l` -eq 0 ]; then
+     if [ `df | grep ${mp} | wc -l` -eq 0 ]; then
        echo "`date`: /cvmfs/${mp} is DOWN"
-       umount -l /cvmfs/${mp}
-       rmdir /cvmfs/${mp}
-       echo `date && ls -lrt /cvmfs`
+       if [ ! -f /dev/shm/unmounting.lck ]; then
+         umount -l /cvmfs/${mp}
+         rmdir /cvmfs/${mp}
+         echo `date && ls -lrt /cvmfs`
 
-       mkdir -p /cvmfs/${mp}
-       mount -t cvmfs ${mp} /cvmfs/${mp}
-       rc=$?
-       if [ ${rc} -eq 0 ] ; then
-         echo "`date`: INFO: Mounted /cvmfs/${mp}" | tee -a /cvmfs/cvmfs-pod.log
-       else
-         echo "`date`: ERROR: Failed to mount $mp"  | tee -a /cvmfs/cvmfs-pod.log
+         mkdir -p /cvmfs/${mp}
+         mount -t cvmfs ${mp} /cvmfs/${mp}
+         rc=$?
+         if [ ${rc} -eq 0 ] ; then
+           echo "`date`: INFO: Mounted /cvmfs/${mp}" | tee -a /cvmfs/cvmfs-pod.log
+         else
+           echo "`date`: ERROR: Failed to mount $mp"  | tee -a /cvmfs/cvmfs-pod.log
+         fi
+         sleep 5
        fi
-       sleep 5
     else
       echo "`date`: /cvmfs/${mp} is OK"
     fi
   done
 	sleep 10
 done
-
-# echo "`date`: Sleeping"
-# sleep infinity
 
